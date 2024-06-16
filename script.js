f77940c8d02c8c31ab35087165f45896a7ce8345
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-06-16 07:30:37 +0300

    basic structure

---
+++ script.js
@@ -0,0 +1,56 @@
+const KEY = 'baac031bcdaa4ae8b7b05039241606'; // :<
+
+let themeToggle = document.getElementById('theme-toggle');
+let storedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+if (storedTheme) {
+    document.documentElement.setAttribute('data-theme', storedTheme)
+    if (storedTheme === 'dark') {
+        themeToggle.checked = true;
+    }
+    else {
+        themeToggle.checked = false;
+    }
+}
+themeToggle.addEventListener('click', function () {
+    let currentTheme = document.documentElement.getAttribute('data-theme');
+    let targetTheme = 'light';
+    if (currentTheme === 'light') {
+        targetTheme = 'dark';
+    }
+    document.documentElement.setAttribute('data-theme', targetTheme);
+    localStorage.setItem('theme', targetTheme);
+});
+// theme switch
+
+let unitToggle = document.getElementById('unit-toggle');
+let storedUnit = localStorage.getItem('unit') || 'c';
+if (storedUnit) {
+    document.documentElement.setAttribute('data-unit', storedUnit)
+    if (storedUnit === 'f') {
+        unitToggle.checked = true;
+    }
+    else {
+        unitToggle.checked = false;
+    }
+}
+unitToggle.addEventListener('click', function () {
+    let currentUnit = document.documentElement.getAttribute('data-unit');
+    let targetUnit = 'c';
+    if (currentUnit === 'c') {
+        targetUnit = 'f';
+    }
+    document.documentElement.setAttribute('data-unit', targetUnit);
+    localStorage.setItem('unit', targetUnit);
+});
+// unit switch
+
+async function getWeather(location) {
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&&q=${location}&days=3&aqi=no&alerts=no`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
