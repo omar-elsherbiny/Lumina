ad90907f7eac96c0cc5462724de1198982e5694d
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-06-20 13:27:49 +0300

    scroll animation prototype

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const KEY = 'baac031bcdaa4ae8b7b05039241606'; // :<
+const currentInfo = document.getElementById('current-info');
 
 let themeToggle = document.getElementById('theme-toggle');
 let storedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
@@ -55,7 +56,27 @@ async function getWeather(location) {
     }
 }
 
-const current = document.getElementById('current');
-current.addEventListener('scroll', e => {
-    // document.getElementById('night-info').scrollIntoView({behavior: "auto", block: "end", inline: "nearest"});
-});
+function getScrollPercentage(element) {
+    const { scrollHeight, clientHeight, scrollTop } = element;
+    const scrolledPortion = scrollHeight - clientHeight - scrollTop;
+    const scrollPerc = scrolledPortion / (scrollHeight - clientHeight);
+    return 1 - Math.max(Math.min(scrollPerc, 1), 0);
+}
+
+function lerp(a, b, t, capped = true) {
+    if (capped) {
+        t = Math.max(Math.min(t, 1), 0);
+    }
+    return ((1 - t) * a + t * b).toFixed(1);
+}
+
+function updateCurrentInfo() {
+    let t = getScrollPercentage(currentInfo);
+    currentInfo.style.backgroundColor = `color-mix(in lab, red ${lerp(0, 100, t)}%, blue)`;
+    document.getElementById('day').style.color = `rgba(0,0,0,${lerp(1, 0, 2 * t)})`;
+    document.getElementById('night').style.color = `rgba(0,0,0,${lerp(0, 1, 2 * t)})`;
+}
+
+updateCurrentInfo();
+currentInfo.addEventListener('scroll', updateCurrentInfo);
+// scroll animation
